c2e450d5a5ef2b1b00aca81cd4684bd432a12efd
kouta <pond140529@gmail.com>
2024-07-25 01:28:57 +0900

    +Mypage.js

--- components/MyPage.js
+++ components/MyPage.js
@@ -1,38 +1,38 @@
 import React, { useState } from 'react';
-import { View, Text, Image, StyleSheet, TouchableOpacity, TextInput, ScrollView } from 'react-native';
+import { View, Text, Image, StyleSheet, TouchableOpacity, TextInput, ScrollView, KeyboardAvoidingView, Platform  } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 import RNPickerSelect from 'react-native-picker-select';
 
 const countries = [
-  { label: 'Japan', value: 'JP' },
-  { label: 'United States', value: 'US' },
-  { label: 'Canada', value: 'CA' },
-  { label: 'United Kingdom', value: 'GB' },
-  { label: 'Australia', value: 'AU' },
-  { label: 'Germany', value: 'DE' },
-  { label: 'France', value: 'FR' },
-  { label: 'China', value: 'CN' },
-  { label: 'India', value: 'IN' },
-  { label: 'Brazil', value: 'BR' },
-  { label: 'Russia', value: 'RU' },
-  { label: 'South Korea', value: 'KR' },
-  { label: 'Mexico', value: 'MX' },
-  { label: 'Italy', value: 'IT' },
-  { label: 'Spain', value: 'ES' },
-  { label: 'Netherlands', value: 'NL' },
-  { label: 'Sweden', value: 'SE' },
-  { label: 'Norway', value: 'NO' },
-  { label: 'Denmark', value: 'DK' },
-  { label: 'Finland', value: 'FI' },
-  { label: 'Switzerland', value: 'CH' },
-  { label: 'South Africa', value: 'ZA' },
-  { label: 'New Zealand', value: 'NZ' },
-  { label: 'Argentina', value: 'AR' },
-  { label: 'Turkey', value: 'TR' },
-  { label: 'Saudi Arabia', value: 'SA' },
-  { label: 'United Arab Emirates', value: 'AE' },
-  // 他の国も追加できます
+  { label: 'Japan', value: 'Japan' },
+  { label: 'United States', value: 'United States' },
+  { label: 'Canada', value: 'Canada' },
+  { label: 'United Kingdom', value: 'United Kingdom' },
+  { label: 'Australia', value: 'Australia' },
+  { label: 'Germany', value: 'Germany' },
+  { label: 'France', value: 'France' },
+  { label: 'China', value: 'China' },
+  { label: 'India', value: 'India' },
+  { label: 'Brazil', value: 'Brazil' },
+  { label: 'Russia', value: 'Russia' },
+  { label: 'South Korea', value: 'South Korea' },
+  { label: 'Mexico', value: 'Mexico' },
+  { label: 'Italy', value: 'Italy' },
+  { label: 'Spain', value: 'Spain' },
+  { label: 'Netherlands', value: 'Netherlands' },
+  { label: 'Sweden', value: 'Sweden' },
+  { label: 'Norway', value: 'Norway' },
+  { label: 'Denmark', value: 'Denmark' },
+  { label: 'Finland', value: 'Finland' },
+  { label: 'Switzerland', value: 'Switzerland' },
+  { label: 'South Africa', value: 'South Africa' },
+  { label: 'New Zealand', value: 'New Zealand' },
+  { label: 'Argentina', value: 'Argentina' },
+  { label: 'Turkey', value: 'Turkey' },
+  { label: 'Saudi Arabia', value: 'Saudi Arabia' },
+  { label: 'United Arab Emirates', value: 'United Arab Emirates' },
 ];
+
 const MyPageScreen = () => {
   const [username, setusername] = useState('kouta');
   const [name, setName] = useState('Kouta Ikeguchi');
@@ -68,152 +68,151 @@ const MyPageScreen = () => {
   };
 
   return (
-    <ScrollView style={styles.container}>
-      <View style={styles.header}>
-        <TouchableOpacity onPress={pickImage}>
-          <Image source={image ? { uri: image } : require('../assets/avatar.png')} style={styles.avatar} />
-        </TouchableOpacity>
-      
-        {isEditingUsername ? (
-          <TextInput
-            style={[styles.username, styles.input]}
-            value={username}
-            onChangeText={setusername}
-            onBlur={() => setIsEditingUsername(false)}
-          />
-        ) : (
-          <TouchableOpacity onPress={() => setIsEditingUsername(true)}>
-            <Text style={styles.username}>{username}</Text>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      style={{ flex: 1 }}
+    >
+      <ScrollView style={styles.container}>
+        <View style={styles.header}>
+          <TouchableOpacity onPress={pickImage}>
+            <Image source={image ? { uri: image } : require('../assets/avatar.png')} style={styles.avatar} />
           </TouchableOpacity>
-        )}
         
-      </View>
+          {isEditingUsername ? (
+            <TextInput
+              style={[styles.username, styles.textRight,{ borderBottomWidth: 0 }]}//編集モードのときの黒の下線を除去
+              value={username}
+              onChangeText={setusername}
+              onBlur={() => setIsEditingUsername(false)}
+            />
+          ) : (
+            <TouchableOpacity onPress={() => setIsEditingUsername(true)}>
+              <Text style={styles.username}>{username}</Text>
+            </TouchableOpacity>
+          )}
+          
+        </View>
 
-      <View style={styles.infoContainer}>
-        <View style={styles.infoRow}>
-          <Text style={styles.label}>Name</Text>
-          {isEditingName ? (
-            <TextInput
-              style={[styles.input, styles.textRight]}
-              value={name}
-              onChangeText={setName}
-              onBlur={() => setIsEditingName(false)}
-            />
-          ) : (
-            <TouchableOpacity onPress={() => setIsEditingName(true)} style={styles.touchable}>
-              <Text style={[styles.value, styles.textRight]}>{name}</Text>
-            </TouchableOpacity>
-          )}
-        </View>
-        <View style={styles.infoRow}>
-          <Text style={styles.label}>Country</Text>
-          {isEditingCountry ? (
-            <View style={styles.pickerContainer}>
-              <RNPickerSelect
-                onValueChange={(value) => {
-                  setCountry(value);
-                  setIsEditingCountry(false);
-                }}
-                items={countries}
-                style={{
-                  ...pickerSelectStyles,
-                  inputIOS: [pickerSelectStyles.inputIOS, styles.textRight],
-                  inputAndroid: [pickerSelectStyles.inputAndroid, styles.textRight],
-                }}
-                useNativeAndroidPickerStyle={false}
-                placeholder={{}}
-                value={country}
+        <View style={styles.infoContainer}>
+          <View style={styles.infoRow}>
+            <Text style={styles.label}>Name</Text>
+            {isEditingName ? (
+              <TextInput
+                style={[styles.input, styles.textRight,{ borderBottomWidth: 0 }]}//編集モードのときの黒の下線を除去
+                value={name}
+                onChangeText={setName}
+                onBlur={() => setIsEditingName(false)}
               />
-            </View>
-          ) : (
-            <TouchableOpacity onPress={() => setIsEditingCountry(true)} style={styles.touchable}>
-              <Text style={[styles.value, styles.textRight]}>{country}</Text>
-            </TouchableOpacity>
-          )}
-        </View>
-        <View style={styles.infoRow}>
-          <Text style={styles.label}>Affiliation</Text>
-          {isEditingAffiliation ? (
-            <TextInput
-              style={[styles.input, styles.textRight]}
-              value={affiliation}
-              onChangeText={setAffiliation}
-              onBlur={() => setIsEditingAffiliation(false)}
-            />
-          ) : (
-            <TouchableOpacity onPress={() => setIsEditingAffiliation(true)} style={styles.touchable}>
-              <Text style={[styles.value, styles.textRight]}>{affiliation}</Text>
-            </TouchableOpacity>
-          )}
-        </View>
-        <View style={styles.infoRow}>
-          <Text style={styles.label}>Occupation</Text>
-          {isEditingOccupation ? (
-            <View style={styles.pickerContainer}>
-              <RNPickerSelect
-                onValueChange={(value) => {
-                  setOccupation(value);
-                  setIsEditingOccupation(false);
-                }}
-                items={[
-                  { label: 'Ophthalmologist', value: 'Ophthalmologist' },
-                  { label: 'Non-ophthalmologist (medical doctor)', value: 'Non-ophthalmologist (medical doctor)' },
-                  { label: 'Nurse', value: 'Nurse' },
-                  { label: 'Others', value: 'Others' },
-                ]}
-                style={{
-                  ...pickerSelectStyles,
-                  inputIOS: [pickerSelectStyles.inputIOS, styles.textRight],
-                  inputAndroid: [pickerSelectStyles.inputAndroid, styles.textRight],
-                }}
-                useNativeAndroidPickerStyle={false}
-                placeholder={{}}
-                value={occupation}
+            ) : (
+              <TouchableOpacity onPress={() => setIsEditingName(true)} style={styles.touchable}>
+                <Text style={[styles.value, styles.textRight]}>{name}</Text>
+              </TouchableOpacity>
+            )}
+          </View>
+          <View style={styles.infoRow}>
+            <Text style={styles.label}>Country</Text>
+            {isEditingCountry ? (
+              <View style={styles.pickerContainer}>
+                <RNPickerSelect
+                  onValueChange={(value) => setCountry(value)}
+                  onDonePress={() => setIsEditingCountry(false)}
+                  items={countries}
+                  style={{
+                    ...pickerSelectStyles,
+                    inputIOS: [pickerSelectStyles.inputIOS, styles.textRight, {borderWidth: 0 }],
+                    inputAndroid: [pickerSelectStyles.inputAndroid, styles.textRight, {borderWidth: 0 }],
+                  }}
+                  useNativeAndroidPickerStyle={false}
+                  placeholder={{}}
+                  value={country}
+                />
+              </View>
+            ) : (
+              <TouchableOpacity onPress={() => setIsEditingCountry(true)} style={styles.touchable}>
+                <Text style={[styles.value, styles.textRight]}>{country}</Text>
+              </TouchableOpacity>
+            )}
+          </View>
+          <View style={styles.infoRow}>
+            <Text style={styles.label}>Affiliation</Text>
+            {isEditingAffiliation ? (
+              <TextInput
+                style={[styles.input, styles.textRight, {borderBottomWidth : 0 }]} //編集モードのときの黒の下線を除去
+                value={affiliation}
+                onChangeText={setAffiliation}
+                onBlur={() => setIsEditingAffiliation(false)}
               />
-            </View>
-          ) : (
-            <TouchableOpacity onPress={() => setIsEditingOccupation(true)} style={styles.touchable}>
-              <Text style={[styles.value, styles.textRight]}>{occupation}</Text>
-            </TouchableOpacity>
-          )}
+            ) : (
+              <TouchableOpacity onPress={() => setIsEditingAffiliation(true)} style={styles.touchable}>
+                <Text style={[styles.value, styles.textRight]}>{affiliation}</Text>
+              </TouchableOpacity>
+            )}
+          </View>
+          <View style={styles.infoRow}>
+            <Text style={styles.label}>Occupation</Text>
+            {isEditingOccupation ? (
+              <View style={styles.pickerContainer}>
+                <RNPickerSelect
+                  onValueChange={(value) => setOccupation(value)}
+                  onDonePress={() => setIsEditingOccupation(false)}
+                  items={[
+                    { label: 'Ophthalmologist', value: 'Ophthalmologist' },
+                    { label: 'Non-ophthalmologist (medical doctor)', value: 'Non-ophthalmologist (medical doctor)' },
+                    { label: 'Nurse', value: 'Nurse' },
+                    { label: 'Others', value: 'Others' },
+                  ]}
+                  style={{
+                    ...pickerSelectStyles,
+                    inputIOS: [pickerSelectStyles.inputIOS, styles.textRight, {borderWidth: 0 }],
+                    inputAndroid: [pickerSelectStyles.inputAndroid, styles.textRight, {borderWidth: 0 }],
+                  }}
+                  useNativeAndroidPickerStyle={false}
+                  placeholder={{}}
+                  value={occupation}
+                />
+              </View>
+            ) : (
+              <TouchableOpacity onPress={() => setIsEditingOccupation(true)} style={styles.touchable}>
+                <Text style={[styles.value, styles.textRight]}>{occupation}</Text>
+              </TouchableOpacity>
+            )}
+          </View>
+          <View style={styles.infoRow}>
+            <Text style={styles.label}>Experience</Text>
+            {isEditingExperience ? (
+              <View style={styles.pickerContainer}>
+                <RNPickerSelect
+                  onValueChange={(value) => setExperience(value)}
+                  onDonePress={() => setIsEditingExperience(false)}
+                  items={[
+                    { label: '0 ~ 5', value: '0 ~ 5' },
+                    { label: '5 ~ 10', value: '5 ~ 10' },
+                    { label: '10 ~ 15', value: '10 ~ 15' },
+                    { label: '15 ~ 20', value: '15 ~ 20' },
+                    { label: '20+', value: '20+' },
+                  ]}
+                  style={{
+                    ...pickerSelectStyles,
+                    inputIOS: [pickerSelectStyles.inputIOS, styles.textRight, {borderWidth: 0 }],
+                    inputAndroid: [pickerSelectStyles.inputAndroid, styles.textRight, {borderWidth: 0 }],
+                  }}
+                  useNativeAndroidPickerStyle={false}
+                  placeholder={{}}
+                  value={experience}
+                />            
+              </View>
+            ) : (
+              <TouchableOpacity onPress={() => setIsEditingExperience(true)} style={styles.touchable}>
+                <Text style={[styles.value, styles.textRight]}>{experience}</Text>
+              </TouchableOpacity>
+            )}
+          </View>
+          <TouchableOpacity>
+            <Text style={styles.logoutButton}>Logout</Text>
+          </TouchableOpacity>
         </View>
-        <View style={styles.infoRow}>
-          <Text style={styles.label}>Experience</Text>
-          {isEditingExperience ? (
-            <View style={styles.pickerContainer}>
-              <RNPickerSelect
-                onValueChange={(value) => {
-                  setExperience(value);
-                  setIsEditingExperience(false);
-                }}
-                items={[
-                  { label: '0 ~ 5', value: '0 ~ 5' },
-                  { label: '5 ~ 10', value: '5 ~ 10' },
-                  { label: '10 ~ 15', value: '10 ~ 15' },
-                  { label: '15 ~ 20', value: '15 ~ 20' },
-                  { label: '20+', value: '20+' },
-                ]}
-                style={{
-                  ...pickerSelectStyles,
-                  inputIOS: [pickerSelectStyles.inputIOS, styles.textRight],
-                  inputAndroid: [pickerSelectStyles.inputAndroid, styles.textRight],
-                }}
-                useNativeAndroidPickerStyle={false}
-                placeholder={{}}
-                value={experience}
-              />
-            </View>
-          ) : (
-            <TouchableOpacity onPress={() => setIsEditingExperience(true)} style={styles.touchable}>
-              <Text style={[styles.value, styles.textRight]}>{experience}</Text>
-            </TouchableOpacity>
-          )}
-        </View>
-        <TouchableOpacity>
-          <Text style={styles.logoutButton}>Logout</Text>
-        </TouchableOpacity>
-      </View>
-    </ScrollView>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 };
 
